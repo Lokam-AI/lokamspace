2049e9913aefd39c449750a45efd47c5e46f4d36
Ashwin <ashes192000@gmail.com>
2025-06-20 16:34:34 +0000

    Removed Trailing / on allowed origins

--- server/app.py
+++ server/app.py
@@ -19,7 +19,7 @@ app = FastAPI(
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://192.168.2.18:3000", "https://dev-autopulse.lokam.ai/", "https://autopulse.lokam.ai/"],
+    allow_origins=["http://localhost:3000", "http://192.168.2.18:3000", "https://dev-autopulse.lokam.ai", "https://autopulse.lokam.ai"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
